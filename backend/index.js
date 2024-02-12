c4fc19a4858213d3139a759793638116cfdf8cce
Sahil <leviack629@gmail.com>
2024-02-12 13:28:15 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,20 @@
 // index.js
-const express = require('express');
-const { default: mongoose } = require('mongoose');
+const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
-// const authRoutes = require('./routes/auth');
-// const contactRoutes = require('./routes/contact');
+const dotenv = require("dotenv");
 
-// app.use(express.json());
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("db connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// // Routes
-// app.use('/api/auth', authRoutes);
-// app.use('/api/contact', contactRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-const connectDB=async()=>{
-    try {
-        await mongoose.connect
-    } catch (error) {
-        console.log(error);
-    }
-}
-app.listen(5000,()=>{
-    console.log("server started");
-})
+dotenv.config();
+app.listen(5000, () => {
+  connectDB();
+  console.log("server started");
+});
